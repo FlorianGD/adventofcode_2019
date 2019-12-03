742b488af305aebd98cde6c34826de6ab6df7698
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2019-12-03 10:13:48 +0100

    day03 part 1

---
+++ day03.py
@@ -0,0 +1,48 @@
+"""Advent of code day 3: Crossed Wires"""
+from typing import List, Tuple, Set, Dict
+from pathlib import Path
+
+puzzle: str = Path("day03_input.txt").read_text()
+
+
+class Wire:
+    moves: Dict[str, complex] = {"U": 1j, "D": -1j, "R": 1, "L": -1}
+
+    def __init__(self, directions: List[Tuple[str, int]]):
+        self.directions: List[Tuple[str, int]] = directions
+        self.path: List[complex] = [0]
+        self.positions: Set[complex] = set(self._compute_path())
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(directions={self.directions[:5]},...)"
+
+    def _compute_path(self) -> List[complex]:
+        for move, length in self.directions:
+            from_ = self.path[-1]
+            direction = self.moves[move]
+            for i in range(1, length + 1):
+                self.path.append(from_ + i * direction)
+        return self.path
+
+    def __and__(self, other) -> Set[complex]:
+        return self.positions & other.positions
+
+
+def parse_input(puzzle: str) -> List[Wire]:
+    directions = [
+        [(d[0], int(d[1:])) for d in line.split(",")] for line in puzzle.splitlines()
+    ]
+    return [Wire(d) for d in directions]
+
+
+def manhattan_distance(position: complex) -> int:
+    return int(abs(position.real) + abs(position.imag))
+
+
+def first_non_zero_intersection(wire1: Wire, wire2: Wire) -> int:
+    intersections = wire1 & wire2
+    intersections.remove(0)
+    return min([manhattan_distance(p) for p in intersections])
+
+
+print(f"Solution for part 1: {first_non_zero_intersection(*parse_input(puzzle))}")
