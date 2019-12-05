d20b32ad3555170a98e02ad585d3ccc5945ac952
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2019-12-05 10:16:44 +0100

    day05 part 1

---
+++ day05.py
@@ -0,0 +1,75 @@
+"""AoC 2019 day 5: Sunny with a Chance of Asteroids"""
+
+from pathlib import Path
+from typing import List, Tuple
+from collections import namedtuple
+
+Instruction = namedtuple("Instruction", "opcode mode1 mode2 mode3")
+
+opcodes = {
+    99: "end",
+    1: "add",
+    2: "mul",
+    3: "set_input",
+    4: "set_output",
+    5: "jump-if-true",
+    6: "jump-if-false",
+    7: "less than",
+    8: "equals",
+}
+
+
+def read_input() -> List[int]:
+    puzzle_input = Path("./day05_input.txt").read_text()
+    return [int(x) for x in puzzle_input.split(",")]
+
+
+def get_instruction(param: int) -> Instruction:
+    opcode = opcodes[param % 100]
+    mode1 = "immediate" if param // 100 % 10 else "position"
+    mode2 = "immediate" if param // 1000 % 10 else "position"
+    mode3 = "immediate" if param // 10000 % 10 else "position"
+    return Instruction(opcode, mode1, mode2, mode3)
+
+
+def value_from_mode(intcodes: List[int], mode: str, val: int) -> int:
+    if mode == "position":
+        return intcodes[val]
+    else:
+        return val
+
+
+def operate(intcodes: List[int], pointer: int = 0, input_: int = 1) -> List[int]:
+    instr = get_instruction(intcodes[pointer])
+    if instr.opcode == "end":
+        return intcodes
+    # 1 parameter instructions
+    target = intcodes[pointer + 1]
+
+    if instr.opcode == "set_input":
+        intcodes[target] = input_
+        return operate(intcodes, pointer + 2, input_=input_)
+    target = value_from_mode(intcodes, instr.mode1, target)
+    if instr.opcode == "set_output":
+        print(f"output: {target}")
+        return operate(intcodes, pointer + 2, input_=input_)
+    if instr.opcode == "jump-if-true":
+        pass
+    # 3 parameters instructions
+    a = value_from_mode(intcodes, instr.mode1, intcodes[pointer + 1])
+    b = value_from_mode(intcodes, instr.mode2, intcodes[pointer + 2])
+    target = intcodes[pointer + 3]
+
+    if instr.opcode == "add":
+        intcodes[target] = a + b
+        return operate(intcodes, pointer + 4, input_=input_)
+    if instr.opcode == "mul":
+        intcodes[target] = a * b
+        return operate(intcodes, pointer + 4, input_=input_)
+
+    raise ValueError(f"opcode {instr.opcode} not recognised.")
+
+
+# operate([1002, 4, 3, 4, 33])
+puzzle = read_input()
+operate(puzzle, input_=1)
