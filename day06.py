e0b167d2631b3d9dbee312edaaae3d221ee06368
Florian Gaudin-Delrieu <fgaudindelrieu@idmog.com>
2019-12-06 10:10:29 +0100

    day06 part 2

--- day06.py
+++ day06.py
@@ -3,7 +3,7 @@ import networkx as nx
 from typing import List, Tuple
 from pathlib import Path
 from networkx.algorithms.dag import ancestors
-
+from networkx.algorithms.shortest_paths.generic import shortest_path
 
 def puzzle_to_graph() -> nx.DiGraph:
     puzzle = Path("./day06_input.txt").read_text().splitlines()
@@ -19,3 +19,9 @@ def length_of_each_nodes(graph: nx.DiGraph) -> int:
 
 G = puzzle_to_graph()
 print(f"Solution for part 1: {length_of_each_nodes(G)}")
+
+# We are required to find the shortest path between YOU and SAN. The networkx function
+# returns a list of nodes, including YOU and SAN, so we have to count them out. We
+# further need to withdraw one because we want to go one step before SAN, not to SAN
+# exactly. This explains the "-3" below.
+print(f"Solution for part 2: {len(shortest_path(nx.Graph(G), 'YOU', 'SAN')) - 3}")
